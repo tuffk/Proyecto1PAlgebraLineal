a5627ab81cc037a9045ed6d94990c99d3f6e6bad
tuffk <tuffkmulhall@gmail.com>
2015-02-06 12:46:19 -0600

    crecion del proeycto
    primeros archivos de visual

---
+++ try1-AL1P/try1-AL1P/main.cpp
@@ -0,0 +1,25 @@
+#include<cstdlib>
+#include<iostream>
+#include<vector>
+
+//#define ABC { 'a', 'b','c','d','e','f', 'g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z', ' '}
+
+using namespace std;
+
+vector<char> ABC{ 'a', 'b','c','d','e','f', 'g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z', ' '};
+/*
+	27 caracteres en el ABC
+
+*/
+
+
+
+
+
+int main()
+{
+
+	
+	system("PAUSE");
+	return 0;
+}
