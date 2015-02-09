0fc28067016445863b0fd659f2073f5a4ce220a4
Marcos <marcos.lopezmendoza@gmail.com>
2015-02-09 14:30:26 -0600

    desorden
    patrocinado por marcos

--- try1-AL1P/try1-AL1P/main.cpp
+++ try1-AL1P/try1-AL1P/main.cpp
@@ -5,6 +5,7 @@
 #include<math.h>
 
 //#define ABC { 'a', 'b','c','d','e','f', 'g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z', ' '}
+//TODA VARIABLE CON NOMBRE VULGAR ES HECHO POR JAIME
 
 using namespace std;
 
@@ -17,6 +18,8 @@ vector<char> ABC{ 'a', 'b','c','d','e','f', 'g','h','i','j','k','l','m','n','ñ',
 	con ascii seria mod 128
 */
 
+int pos(char caracter);
+vector<vector<double>> invertir(vector<vector<int>>);
 
 void multKcolum(vector<vector<double>> mat, vector<double>&col)
 {
@@ -33,25 +36,27 @@ void multKcolum(vector<vector<double>> mat, vector<double>&col)
 	return;
 }
 
-void empaquetamela(vector<vector<double>>& p, vector<char> vagina)
+void empaquetamela(vector<vector<double>>& p, vector<char> kuz)
 {
 	int x = 0;
 	vector<double> temp;
-	int tam = ceil(vagina.size()/3);
+	int tam = ceil(kuz.size()/3);
 	for (int i = 0; i <= tam; i++)
 	{
 		temp.clear();
 		for (int j = 0; j < 3; j++)
 		{
-			//p[i][j] = vagina[j + i];
-			if (!vagina.empty())
+			//p[i][j] = kuz[j + i];
+			if (!kuz.empty())
 			{
-				x = vagina[0];
-				vagina.erase(vagina.begin());
+				/*x = kuz[0];
+				kuz.erase(kuz.begin());*/
+				x = pos(kuz[0]);
+				kuz.erase(kuz.begin());
 			}
 			else
 			{
-				x = 0;
+				x = 27;
 			}
 			
 
@@ -73,6 +78,24 @@ void empaquetamela(vector<vector<double>>& p, vector<char> vagina)
 }
 
 
+vector<char> aletreame(vector<vector<double>> tutzke)
+{
+	vector<char> sharmuta;
+	for each (vector<double> modishne in tutzke)
+	{
+
+		for (int i = 0; i < 3; i++)
+		{
+
+			sharmuta.push_back(ABC[modishne[i]]);
+
+		}
+
+	}
+	return sharmuta;
+}
+
+
 int main()
 {
 	vector<vector<char>> llave; //matriz que se utiliza para encriptar y des encriptar 
@@ -81,8 +104,8 @@ int main()
 	*/
 	vector<vector<double>> test = { { 5, 17, 20 }, { 9, 23, 3 }, { 2, 11, 13 } };
 	vector<double> test2 = { 1, 2, 3 };
-	multKcolum(test, test2);
-	test2;
+/*	multKcolum(test, test2);
+	test2;*/
 
 	vector<char> mila;
 	string tujefa;
@@ -92,31 +115,68 @@ int main()
 		mila.push_back(var);
 	}
 	mila;
-	vector<vector<double>> pito;
+	vector<vector<double>> zain;
 
-	empaquetamela(pito, mila);
-	//pito;
+	empaquetamela(zain, mila);
+	//zain;
 
-	/*for each (vector<int> var in pito)
+	/*for each (vector<int> var in zain)
 	{
 		multKcolum(test, var);
 	}*/
-	for (int i = 0; i < pito.size(); i++)
+	for (int i = 0; i < zain.size(); i++)
 	{
-		multKcolum(test, pito[i]);
+		multKcolum(test, zain[i]);
 	}
-	pito; // la multiplicacion para encriptar funge falta modulear
+	zain; // la multiplicacion para encriptar funge falta modulear
 	//moduleamos
-	for (int i = 0; i < pito.size(); i++)
+	for (int i = 0; i < zain.size(); i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			pito[i][j] = ((int)pito[i][j]) % 128;
+			zain[i][j] = ((int)zain[i][j]) % 28;
 		}
 	}
-	pito;
+	zain;
 	vector<vector<double>> inversa = { { 0.5288270, -0.0019881, -0.8131213 }, { -0.2206759, 0.0497018, 0.3280318 }, { 0.1053678, -0.0417495, -0.0755467 } };
 	
+	mila= aletreame(zain);
+	for each (char var in mila)
+	{
+		cout << var;
+	}
 	system("PAUSE");
 	return 0;
 }
+
+int pos(char caracter)
+{
+
+	for (int i = 0; i < ABC.size(); i++)
+	{
+		if (caracter == ABC[i])
+			return i;
+	}
+
+	return -1;
+
+}
+
+//vector<vector<double>> invertir(vector<vector<double>> mat)
+//{
+//
+//	vector<vector<double>> inv;
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		vector<double> temp;
+//		for (int j = 0; j < 3; j++)
+//		{
+//			for (int k = 0; k < 3; k++)
+//			{
+//
+//			}
+//		}
+//	}
+//
+//}
